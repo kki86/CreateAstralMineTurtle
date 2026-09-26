afde35bea862da382cca242f5e7b566b57a72355
Kainalu Ishikawa <kki86@hawaii.edu>
2026-09-26 18:58:46 +0900

    Update 4x4Mine.lua

--- 4x4Mine.lua
+++ 4x4Mine.lua
@@ -37,16 +37,16 @@ local function forward()
     end
 end
 
--- Move up while digging
-local function up()
+-- Move down while digging
+local function down()
     if not checkFuel() then
         error("Out of fuel!")
     end
 
-    turtle.digUp()
+    turtle.digDown()
 
-    while not turtle.up() do
-        turtle.digUp()
+    while not turtle.down() do
+        turtle.digDown()
         sleep(0.1)
     end
 end
@@ -83,6 +83,26 @@ local function mineLayer()
     end
 end
 
+-- Return to starting position of the layer
+local function returnToStart()
+
+    turnAround()
+
+    -- Move across the last row
+    for i = 1, SIZE - 1 do
+        forward()
+    end
+
+    -- Move back to the first row
+    turtle.turnLeft()
+
+    for i = 1, SIZE - 1 do
+        forward()
+    end
+
+    turtle.turnRight()
+end
+
 -- Initial refuel
 refuel()
 
@@ -90,32 +110,19 @@ if not checkFuel() then
     return
 end
 
--- Mine 4 layers
+-- Mine 4 layers downward
 for layer = 1, SIZE do
 
     print("Mining layer " .. layer .. " of " .. SIZE)
 
     mineLayer()
 
+    -- Return to the starting corner
+    returnToStart()
+
+    -- Move down to the next layer
     if layer < SIZE then
-
-        -- Return to starting position
-        turnAround()
-
-        for i = 1, SIZE - 1 do
-            forward()
-        end
-
-        turtle.turnLeft()
-
-        for i = 1, SIZE - 1 do
-            forward()
-        end
-
-        turtle.turnRight()
-
-        -- Move up to next layer
-        up()
+        down()
     end
 end
 
